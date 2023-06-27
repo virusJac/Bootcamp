cc8c84720112dfc6983460961e4ea3dd6a1e6fbb
Jarol G. AVILES CRUZ <>
2023-06-26 21:43:13 -0400

    move file to new location: clase6

---
+++ src/clase5/Test.java
@@ -0,0 +1,58 @@
+package clase5;
+/*
+    Recomendacion para probar cada ejercicio 
+    quitar los comentarios de dicho ejercicio
+    @author VirusJac 
+    @version 1
+    
+*/
+import javax.swing.JOptionPane;
+import java.util.Random;
+
+public class Test {
+
+    public static void main(String[] args) {
+        
+//        --- Prueba ejercicio 1 clase 5 --- 
+//        String nombre = JOptionPane.showInputDialog(null, "Ingrese la edad de su mascota", "Entrada", JOptionPane.QUESTION_MESSAGE);
+//        EdadPerro millasPrueba = new EdadPerro(Integer.parseInt(nombre));
+//        JOptionPane.showMessageDialog(null,"Edad en años perros tiene " + millasPrueba.getEdadEnperro(), "Mensaje", JOptionPane.INFORMATION_MESSAGE);
+//        
+        
+
+//        --- Prueba ejercicio 2 clase 5 ---         
+//        String nombre = JOptionPane.showInputDialog(null, "Ingrese la distancia en millas", "Entrada", JOptionPane.QUESTION_MESSAGE);
+//        ConversorMillasKilometros millasPrueba = new ConversorMillasKilometros(Integer.parseInt(nombre));
+//        JOptionPane.showMessageDialog(null,millasPrueBa.getDistaciaMillas() + " Millas es " + millasPrueba.getDistanciaKilometros() +" Km", "Mensaje", JOptionPane.INFORMATION_MESSAGE);
+
+
+
+//        --- Prueba ejercicio 3 clase 5 --- 
+//        String precioOriginal = JOptionPane.showInputDialog(null, "Ingrese el precio original", "Entrada", JOptionPane.QUESTION_MESSAGE);
+//        String porcentajeDesc = JOptionPane.showInputDialog(null, "Ingrese el descuento en porcentaje(sin el signo %)", "Entrada", JOptionPane.QUESTION_MESSAGE);
+//        CalculadoraDescuento calculoTest = new CalculadoraDescuento(Integer.parseInt(precioOriginal), Integer.parseInt(porcentajeDesc));
+//        JOptionPane.showMessageDialog(null,"el precio final  es de " + calculoTest.getPrecioFinal() + " Bs.", "Mensaje", JOptionPane.INFORMATION_MESSAGE);
+
+
+
+//        ---Prueba ejercicio 4 clase 6 ---
+//        String totalCuenta = JOptionPane.showInputDialog(null, "Ingrese el total de la cuenta", "Entrada", JOptionPane.QUESTION_MESSAGE);
+//        String porcentajePropina = JOptionPane.showInputDialog(null, "Ingrese el porcentaje de propina que desea dejar(sin el signo %)", "Entrada", JOptionPane.QUESTION_MESSAGE);
+//        CalculadoraPropinas propinaTest = new CalculadoraPropinas(Integer.parseInt(totalCuenta), Integer.parseInt(porcentajePropina));
+//        JOptionPane.showMessageDialog(null,"El monto seria de " + propinaTest.getMontoPropina()+ " Bs.", "Mensaje", JOptionPane.INFORMATION_MESSAGE);
+        
+
+
+//        ---Prueba ejercicio 5 clase 5 ---        
+//        adivinaNumero testNumero = new adivinaNumero();
+//        String entrada;
+//        int numero;
+//        String mensaje;
+//        do {
+//            entrada = JOptionPane.showInputDialog(null, "Introduce un numero entre 1-100", "Entrada", JOptionPane.QUESTION_MESSAGE);
+//            numero = Integer.parseInt(entrada);
+//            mensaje = testNumero.adivinarNumero(numero);
+//            JOptionPane.showMessageDialog(null, mensaje, "Mensaje", JOptionPane.INFORMATION_MESSAGE);
+//        } while (numero != 0 && !mensaje.startsWith("¡Has adivinado"));
+   }
+}
